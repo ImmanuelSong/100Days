75dd7ed40215f79325649903decd1b0678ac20a1
ImmanuelSong <immanewlsong@gmail.com>
2023-06-08 07:54:02 +0900

    add day 5 and day 6

--- 100Days/day4 clone coding/src/App.jsx
+++ 100Days/day4 clone coding/src/App.jsx
@@ -1,26 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const url = "http://api.coinpaprika.com/v1/tickers";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState();
+  const [selected, setSelected] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  let price;
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
 
   return (
-    <>
-      <div className="w-[350px] h-[150px] bg-white rounded-sm border-black flex flex-col justify-center items-center">
-        <h2 className="text-yellow-600">The Count is : </h2>
-        <span className="text-slate-500">{count}</span>
-        <button
-          className="rounded-full bg-slate-900 text-white w-1/2"
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Click if you want
-        </button>
-      </div>
-    </>
+    <div>
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select onChange={handleSelect}>
+          {coins.map((coin) => (
+            <option key={coin.id}>{coin.name}</option>
+          ))}
+        </select>
+      )}
+      <label htmlFor="usd">USD</label>
+      <input
+        type="number"
+        onChange={(e) => {
+          e.preventDefault();
+          setUsd(e.target.value);
+        }}
+        value={usd}
+        placeholder="How much dollars?"
+      />
+
+      <label htmlFor="coins">{selected}</label>
+      {coins.map((coin) => {
+        if (coin.name === selected) {
+          price = coin.quotes.USD.price;
+        }
+      })}
+      <input type="number" value={usd / price} placeholder="is Equal to" />
+    </div>
   );
 }
-
 export default App;
