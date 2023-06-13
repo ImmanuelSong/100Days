aa0cd1968584e6ecc60b4bfa113d43bb3f624ccf
ImmanuelSong <immanewlsong@gmail.com>
2023-06-13 13:31:31 +0900

    change theme color

--- 100Days/day9 Weather App/src/component/Input.jsx
+++ 100Days/day9 Weather App/src/component/Input.jsx
@@ -14,17 +14,11 @@ const Input = () => {
     setInput("");
   };
 
-  const handleClick = () => {
-    setCitySet(false);
-    setCity("");
-    setInput("");
-  };
-
   return (
     <div className="flex flex-col gap-5 items-center justify-center">
       <form onSubmit={handleSubmit} className="flex justify-between gap-2">
         <input
-          className="w-[90%] h-[40px] shadow-lg uppercase rounded-lg pr-3 text-center border-2 text-red-400 placeholder-red-300 border-red-300 focus:outline-none focus:border-red-500 caret-red-400"
+          className="w-[90%] h-[40px] shadow-lg uppercase rounded-lg pr-3 text-center border-2 text-emerald-700 placeholder-emerald-300 border-emerald-300 focus:outline-none focus:border-emerald-500 caret-emerald-400"
           type="text"
           placeholder="Enter a city name..."
           value={input}
@@ -33,7 +27,7 @@ const Input = () => {
           }}
         />
         <button type="submit" onClick={handleSubmit}>
-          <HiRocketLaunch className="text-[30px] text-red-500 hover:scale-125 transition-all ease duration-500" />
+          <HiRocketLaunch className="text-[30px] text-emerald-500 hover:scale-110 transition-all ease duration-200" />
         </button>
       </form>
       {citySet ? <WeatherBox city={city} /> : null}
--- 100Days/day9 Weather App/src/component/WeatherBox.jsx
+++ 100Days/day9 Weather App/src/component/WeatherBox.jsx
@@ -8,9 +8,9 @@ import mist from "./images/mist.png";
 import rain from "./images/rain.png";
 import snow from "./images/snow.png";
 
-const api = "YOURAPI";
+const api = "82c677021bf925efb5d63d7bd94e7a96";
 
-const WeatherBox = (city) => {
+const WeatherBox = ({ city }) => {
   const [weatherData, setWeatherData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [image, setImage] = useState("");
@@ -18,7 +18,7 @@ const WeatherBox = (city) => {
   const getData = async () => {
     const data = await (
       await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city.city}&units=metric&appid=${api}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`
       )
     ).json();
 
@@ -57,30 +57,34 @@ const WeatherBox = (city) => {
   return (
     <div>
       {loading ? (
-        <HiSun className="animate-ping text-5xl text-red-300" />
+        <HiSun className="animate-ping text-5xl text-emerald-300" />
       ) : (
-        <div className="w-[400px] h-[500px] bg-transparent rounded-lg shadow-lg shadow-red-100 flex flex-col justify-center items-center mt-10 border-solid border-2 border-red-200">
-          <div className="fixed w-[150px] h-[150px] rounded-full bg-red-50 top-44 flex justify-center items-center border-dashed border-2 border-red-200">
+        <div className="w-[400px] h-[500px] bg-transparent rounded-lg shadow-lg shadow-emerald-100 flex flex-col justify-center items-center mt-10 border-solid border-2 border-emerald-200">
+          <div className="fixed w-[150px] h-[150px] rounded-full bg-emerald-50 top-44 grid place-content-center border-dashed border-2 border-emerald-200">
             <img
               src={image}
               alt={weatherData.weather[0].main}
-              className=" aspect-auto w-[100px]"
+              className=" aspect-auto w-[100px] "
             />
           </div>
-          <div className="flex flex-col gap-2  shadow-red-200 shadow-md  w-[50%] h-[200px] translate-y-14 pl-2">
-            <div className="flex gap-5 mb-5 text-red-500 text-lg">
+          <div className="flex flex-col gap-1 items-center  shadow-emerald-200 shadow-md  w-[220px] h-[250px] translate-y-14 pl-2 cursor-default">
+            <div className="flex gap-5 mx-auto mb-2 text-emerald-500 text-lg uppercase">
+              <span className="hover:font-bold">{weatherData.name}</span>
+            </div>
+
+            <div className="flex justify-between gap-5 mb-5 text-emerald-500 text-lg w-[90%] hover:border-2 hover:border-dashed hover:border-emerald-200 hover:scale-[1.01]">
               <h1>Curr. Temp</h1>
               <span>{weatherData.main.temp} °C</span>
             </div>
-            <div className="flex gap-5 mb-5 text-red-500 text-lg">
+            <div className="flex justify-between gap-5 mb-5 text-emerald-500 text-lg w-[90%] hover:border-2 hover:border-dashed hover:border-emerald-200 hover:scale-[1.01]">
               <h1>Feels Like</h1>
               <span>{weatherData.main.feels_like} °C</span>
             </div>
-            <div className="text-red-500 flex gap-5 mb-5 text-lg">
+            <div className="text-emerald-500 flex justify-between gap-5 mb-5 w-[90%] text-lg hover:border-2 hover:border-dashed hover:border-emerald-200 hover:scale-[1.01]">
               <BsWind className="translate-y-[5px]" />{" "}
               <span>{weatherData.wind.speed} m/s</span>
             </div>
-            <div className="text-red-500 flex gap-5 text-lg relative">
+            <div className="text-emerald-500 flex justify-between gap-5 text-lg w-[90%] relative hover:border-2 hover:border-dashed hover:border-emerald-200 hover:scale-[1.01]">
               <IoIosWater className="translate-y-[5px]" />
               <span>{weatherData.main.humidity} %</span>
             </div>
